3bbca959600f55fd968bec1035e611c89804feb4
ymsaadi <youssefmsaadi95@gmail.com>
2023-11-30 18:29:13 -0500

    first commit

---
+++ src/main/java/com/ymsaadi/gamerecommendationapi/GameRecommendationApiApplication.java
@@ -0,0 +1,13 @@
+package com.ymsaadi.gamerecommendationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameRecommendationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameRecommendationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ymsaadi/gamerecommendationapi/GameRecommendationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ymsaadi.gamerecommendationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameRecommendationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
