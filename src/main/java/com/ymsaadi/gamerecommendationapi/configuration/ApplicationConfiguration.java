6ed0cc2ce84b5d92904cddd8d2f3db537d60628e
ymsaadi <youssefmsaadi95@gmail.com>
2023-12-01 16:04:35 -0500

    Added my own implementation for UserDetailsService, to get user by his username.

---
+++ src/main/java/com/ymsaadi/gamerecommendationapi/configuration/ApplicationConfiguration.java
@@ -0,0 +1,19 @@
+package com.ymsaadi.gamerecommendationapi.configuration;
+
+import com.ymsaadi.gamerecommendationapi.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfiguration {
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
