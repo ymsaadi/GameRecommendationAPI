14a91bbf831ed6c1ff3b3f2609a027ac323485dc
ymsaadi <youssefmsaadi95@gmail.com>
2023-12-06 16:10:31 -0500

    Changed expiration date of JWT token from 1 day to 30 days.

--- src/main/java/com/ymsaadi/gamerecommendationapi/services/JwtService.java
+++ src/main/java/com/ymsaadi/gamerecommendationapi/services/JwtService.java
@@ -47,7 +47,7 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
+                .setExpiration(new Date(System.currentTimeMillis() + (86400000L * 30)))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
